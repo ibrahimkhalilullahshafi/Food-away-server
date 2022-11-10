5278764280a2ca36a3dd3562ee460ca86ef48b7c
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-11-10 16:36:35 +0600

    service data path by id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -15,14 +15,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const serviceCollection = client.db('foodAway').collection('services');
+        const serviceCollection = client.db('foodAway').collection('products');
 
-        app.get('/services', async (req, res) => {
+        app.get('/limitServices', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
         })
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const selectedService = await serviceCollection.findOne(query);
+            res.send(selectedService);
+        });
 
     }
     finally {
