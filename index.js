5948e7fe6094b201fcd6eff4e72ab0fb721e8aea
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-11-10 07:01:54 +0600

    edited product to service

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,6 +7,30 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eaa9w7x.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('foodAway').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
+
 
 app.get("/", (req, res) => {
     res.send("server is running");
