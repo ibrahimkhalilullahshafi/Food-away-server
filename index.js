47b4f82ef4c2fa29af1ce742c5e54bd6b7e6aec0
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-11-10 23:36:06 +0600

    final

--- index.js
+++ index.js
@@ -16,24 +16,72 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('foodAway').collection('products');
+        const orderCollection = client.db('foodAway').collection('orders');
+        const reviewCollection = client.db('foodAway').collection('reviews');
 
         app.get('/limitServices', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
-        })
+        });
+
+
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
+
+
         app.get("/services/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const selectedService = await serviceCollection.findOne(query);
             res.send(selectedService);
+        });
+
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+
+        });
+
+
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+
         });
 
     }
@@ -47,6 +95,7 @@ run().catch(error => console.error(error));
 app.get("/", (req, res) => {
     res.send("server is running");
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on port, ${port}`);
